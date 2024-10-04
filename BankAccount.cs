6d4bf0a8f2d464b7f767faaa7a806663bd3e4503
JeppeMarek <Jeppe_marek@hotmail.com>
2024-10-04 11:10:04 +0200

    Add project files.

---
+++ BankAccount.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBank
+{
+    public class BankAccount
+    {
+        // Backing fields
+        private string name;
+        private double balance;
+        private bool locked;
+        // Properties
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public double Balance { get { return this.balance; } }
+        // Constructers
+        public BankAccount (string name, double balance, bool locked)
+        {
+            this.name = name;
+            this.balance = balance;
+            this.locked = locked;
+        }
+        public BankAccount (string name, double balance) : this("", 0, false)
+        {
+            this.name = name;
+            this.balance = balance;
+        }
+        public BankAccount (double balance) : this("", 0, false)
+        {
+            this.balance = balance;
+            this.name = name;
+        }
+        
+        // Methods
+        public override string ToString()
+        {
+            return $"Name: {name}, Balance: {this.balance}";
+        }
+        public void Deposit(double amount)
+        {
+            if (locked == false)
+            {
+                this.balance += amount;
+                Console.WriteLine("Current balance: " + this.balance);
+            } else
+            {
+                Console.WriteLine("Account is locked");
+            }
+        }
+        public void Withdraw(double amount)
+        {
+            if (locked == false)
+            {
+                if (amount <= this.balance)
+                {
+                    this.balance -= amount;
+                }
+                else
+                {
+                    Console.WriteLine($"\nInsufficient funds:\nCurrent balance: {this.balance}");
+                }
+            }else
+            {
+                Console.WriteLine("Account is locked.");
+            }
+        }
+        public void ChangeLockState()
+        {
+            if (this.locked == false)
+            {
+                this.locked = true;
+                Console.WriteLine("Bank account locked");
+            }
+            else
+            {
+                this.locked = false;
+                Console.WriteLine("Bank account unlocked");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace SimpleBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount bankAccOne = new BankAccount("NemKonto", 500, true);
+            BankAccount bankAccTwo = new BankAccount("Besværlig konto", 5);
+            BankAccount bankAccThree = new BankAccount(5000);
+
+            //Console.WriteLine(bankAccOne.ToString());
+            //Console.WriteLine(bankAccTwo.ToString());
+            //Console.WriteLine(bankAccThree.ToString());
+            //bankAccThree.Deposit(500);
+            //Console.WriteLine(bankAccThree.ToString());
+            //bankAccThree.Withdraw(7000);
+            //Console.WriteLine(bankAccThree.ToString());
+            //bankAccThree.ChangeLockState();
+            //Console.WriteLine(bankAccThree.ToString());
+            BankAccount BobsAccount = new BankAccount(5000);
+            BobsAccount.Name = "Bob";
+            Console.WriteLine(BobsAccount);
+
+
+        }
+    }
+}
